59e34d4bb28143b22dda85c971ea20eb2b5450c1
Whilmer Realigue <106437874+Whilreal0@users.noreply.github.com>
2023-03-25 02:47:58 +0800

    View personnel (#13)
    * search-reset-feature
    
    * added view-details
    
    ---------
    
    Co-authored-by: Alvin Prenda Laroya <53330353+alvinlaroya@users.noreply.github.com>

---
+++ client/src/components/navigation/Personnel/ViewDetails.vue
@@ -0,0 +1,94 @@
+<template>
+  
+  <v-card>
+    <v-toolbar dark>Personnel Data</v-toolbar>
+    <v-container class="pa-5">
+      <v-row>
+        <span>
+          Account Number : <strong>{{ data.accountNo }}</strong>
+        </span>
+      </v-row>
+      <v-row>
+        <span>
+          Rank : <strong> {{ data.rank }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Full Name :
+          <strong
+            >{{ data.fname }} {{ data.mname }} {{ data.lname }}</strong
+          ></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Gender : <strong> {{ data.gender }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Mobile Number : <strong>{{ data.mobile }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Address : <strong> {{ data.address }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Date of Birth : <strong> {{ birthDate }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Civil Status : <strong> {{ data.civilStatus }}</strong></span
+        >
+      </v-row>
+
+      <v-row>
+        <span
+          >Philhealth : <strong> {{ data.philhealth }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Ext Name : <strong> {{ data.extName }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Unit : <strong> {{ data.unit }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Designation : <strong> {{ data.designation }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span
+          >Remarks : <strong>{{ data.remarks }}</strong></span
+        >
+      </v-row>
+    </v-container>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["data" ],
+  
+  computed: {
+    birthDate() {
+      return this.data.dateOfBirth.substr(0, 10);
+    },
+  },
+
+};
+</script>
+
+<style>
+</style>

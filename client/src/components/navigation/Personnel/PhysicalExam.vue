0b62f0ef602b748044c01f7eda7b07cf8b86a035
Alvin <alvinreggaelaroya@gmail.com>
2023-03-25 03:42:21 +0800

    feat: added tabs in dialogs

---
+++ client/src/components/navigation/Personnel/PhysicalExam.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card elevation="0">
+    <v-container class="pa-8">
+      <v-row>
+        <h1>Physical Exam</h1>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+
+  computed: {
+    birthDate() {
+      return this.data.dateOfBirth.substr(0, 10);
+    },
+  },
+};
+</script>
+
+<style></style>

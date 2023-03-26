e33d29b7b157e440fd4c6b43bb7ea8b1878eaa32
Whilreal0 <whil.real@gmail.com>
2023-03-26 22:20:20 +0800

    add table for family history

---
+++ client/src/components/navigation/Personnel/Medical/PersonalHistory.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-container v-for="(item, i) in items" :key="i">
+      <v-row>
+        <h2>PERSONAL HISTORY</h2>
+      </v-row>
+      <v-row>
+        <strong class="pr-5">OB History: </strong>
+        <span class="pr-5"
+          >Have you been Pregnant ?
+          <strong class="text-decoration-underline">{{
+            item.pregnant
+          }}</strong></span
+        >
+        <span class="pr-5"
+          >How many times ?
+          <strong class="text-decoration-underline">{{
+            item.howManyTimes
+          }}</strong></span
+        >
+      </v-row>
+      <v-row>
+        <span class="pr-5"
+          >How many Children do you have ?
+          <strong class="text-decoration-underline">{{
+            item.childrenCount
+          }}</strong></span
+        >
+        <span class="pr-5"
+          >Have you have an Abortion ?
+          <strong class="text-decoration-underline">
+            {{ item.abortionCount }}</strong
+          ></span
+        >
+        <span class="pr-5"
+          >if yes, quantify
+          <strong class="text-decoration-underline">{{
+            item.ifAbortionCount
+          }}</strong></span
+        >
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        pregnant: "No",
+        howManyTimes: "0",
+        childrenCount: "0",
+        abortionCount: "0",
+        ifAbortionCount: "0",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>

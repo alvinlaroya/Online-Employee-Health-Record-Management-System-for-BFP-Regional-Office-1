4b3699c069ec3943444bd5d2dde86e491c199e91
alvinlaroya <alvinreggaelaroya@gmail.com>
2023-04-15 20:46:33 +0800

    fix error api

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  /* baseUrl: 'http://127.0.0.1:8000/api', */
-  baseUrl: 'http://192.168.1.143:8000/api',
+  baseUrl: 'http://localhost:8000/api',
   withCredentials: false,
   headers: {
     'Accept': 'application/json',

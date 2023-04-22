496adffe40ab7af5dc530d898251e7996c2619c8
Whilreal0 <whil.real@gmail.com>
2023-04-22 20:36:05 +0800

    fix-ptnote

--- client/src/components/navigation/Personnel/PtNotes/Adl.vue
+++ client/src/components/navigation/Personnel/PtNotes/Adl.vue
@@ -36,168 +36,100 @@ export default {
         this.items = [
           {
             name: "Bathing",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlBathing === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlBathing
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Toileting",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlToileting === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlToileting
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Eating",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlEating === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlEating
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Upper extermity dressing",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlUpperExtermityDressing === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlUpperExtermityDressing
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Lower extremity dressing",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlLowerExtermityDressing === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlLowerExtermityDressing
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Bed Mobility",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlBedMobility === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlBedMobility
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
             name: "Scoot towards head of head",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlScootTowardsHeadOfHead === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlScootTowardsHeadOfHead
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Scoot towards foot of head",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlScootTowardsFootOfHead === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlScootTowardsFootOfHead
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Side to side scooting",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlSideToSideScooting === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlSideToSideScooting
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Supine to long sitting",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlSupineToLongSitting === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlSupineToLongSitting
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Rolling",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlRolling === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlRolling
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Transfers",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlTransfers === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlTransfers
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Wheelchair to Mat",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlWheelChairToMat === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlWheelChairToMat
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Bed to Wheelchair",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlBedToWheelChair === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlBedToWheelChair
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
           {
-            name: "Ambulation",
-            FullyDependent:
-              this.personnelDetails.ptNotes.adlAmbulation === null
-                ? ""
-                : this.personnelDetails.ptNotes.adlAmbulation
-                ? "Yes"
-                : "No",
-            PartiallyDependent: "",
-            Independent: "",
+            FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+            PartiallyDependent:
+              item.status === "Partially Dependent" ? "Yes" : "",
+            Independent: item.status === "Independent" ? "Yes" : "",
           },
         ];
       },
@@ -207,172 +139,97 @@ export default {
   computed: {
     ...mapGetters(["personnelDetails"]),
   },
+  // tableData() {
+  //   return this.selfCareItems.map((item) => ({
+  //     name: item.name,
+  //     fullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+  //     partiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+  //     independent: item.status === "Independent" ? "Yes" : "",
+  //   }));
+  // },
   created() {
     this.items = [
       {
         name: "Bathing",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlBathing === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlBathing
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Toileting",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlToileting === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlToileting
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Eating",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlEating === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlEating
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Upper extermity dressing",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlUpperExtermityDressing === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlUpperExtermityDressing
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Lower extremity dressing",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlLowerExtermityDressing === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlLowerExtermityDressing
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Bed Mobility",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlBedMobility === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlBedMobility
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
         name: "Scoot towards head of head",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlScootTowardsHeadOfHead === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlScootTowardsHeadOfHead
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Scoot towards foot of head",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlScootTowardsFootOfHead === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlScootTowardsFootOfHead
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Side to side scooting",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlSideToSideScooting === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlSideToSideScooting
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Supine to long sitting",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlSupineToLongSitting === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlSupineToLongSitting
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Rolling",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlRolling === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlRolling
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Transfers",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlTransfers === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlTransfers
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Wheelchair to Mat",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlWheelChairToMat === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlWheelChairToMat
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Bed to Wheelchair",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlBedToWheelChair === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlBedToWheelChair
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
       {
-        name: "Ambulation",
-        FullyDependent:
-          this.personnelDetails.ptNotes.adlAmbulation === null
-            ? "-"
-            : this.personnelDetails.ptNotes.adlAmbulation
-            ? "Yes"
-            : "No",
-        PartiallyDependent: "-",
-        Independent: "-",
+        FullyDependent: item.status === "Fully Dependent" ? "Yes" : "",
+        PartiallyDependent: item.status === "Partially Dependent" ? "Yes" : "",
+        Independent: item.status === "Independent" ? "Yes" : "",
       },
     ];
   },

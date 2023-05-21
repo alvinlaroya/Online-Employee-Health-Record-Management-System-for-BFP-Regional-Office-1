6d130f2a33e94e401a606c75e5fbc2242f5b5b21
Whilreal0 <whil.real@gmail.com>
2023-05-21 22:12:43 +0800

    fix

--- client/src/views/navigation/User-Management/TransactionLog.vue
+++ client/src/views/navigation/User-Management/TransactionLog.vue
@@ -11,36 +11,36 @@
           hide-default-footer
         ></v-data-table>
       </v-card-text>
+
     </v-card>
   </v-container>
 </template>
   
   <script>
 export default {
-  data() {
-    return {
-      headers: [
-        { text: "Name", value: "name" },
-        { text: "Log", value: "log" },
-        { text: "Log Created", value: "logCreated" },
-      ],
-      items: [
-        { name: "John Doe", log: "Added Personnel", logCreated: "2023-05-20" },
-        {
-          name: "Jane Smith",
-          log: "Delete Personnel",
-          logCreated: "2023-05-19",
-        },
-        { name: "Michael Johnson", log: "Logged In", logCreated: "2023-05-18" },
-        {
-          name: "Michael Johnson",
-          log: "Logged Out",
-          logCreated: "2023-05-18",
-        },
-        // Add more items as needed
-      ],
-    };
-  },
+  data: () => ({
+
+    headers: [
+      { text: "Name", value: "name" },
+      { text: "Log", value: "log" },
+      { text: "Log Created", value: "logCreated" },
+    ],
+    items: [
+      { name: "John Doe", log: "Added Personnel", logCreated: "2023-05-20" },
+      {
+        name: "Jane Smith",
+        log: "Delete Personnel",
+        logCreated: "2023-05-19",
+      },
+      { name: "Michael Johnson", log: "Logged In", logCreated: "2023-05-18" },
+      {
+        name: "Michael Johnson",
+        log: "Logged Out",
+        logCreated: "2023-05-18",
+      },
+      // Add more items as needed
+    ],
+  }),
 };
 </script>
   
